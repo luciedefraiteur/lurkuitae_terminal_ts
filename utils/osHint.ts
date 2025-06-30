e19419e1763999fa5220260cf0f4bc3045652be7
Lucie Défraiteur <luciedefraiteur@gmail.com>
2025-06-30 09:46:55 +0200

    clean

--- utils/osHint.ts
+++
@@ -1,5 +0,0 @@
-const isWindows = process.platform === 'win32';
-export const osHint = isWindows
-    ? "(Contexte : Windows, cmd ou PowerShell)"
-    : "(Contexte : Linux ou Unix-like, shell POSIX)";
-
